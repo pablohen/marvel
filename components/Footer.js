d4270369b2331538fb861333ddbb56d7b88a4f96
Pablo Henrique <pablo_hen@hotmail.com>
2021-07-18 12:46:14 -0300

    site footer with marvel api reference

---
+++ components/Footer.js
@@ -0,0 +1,24 @@
+const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <footer className="bg-red-500">
+      <div className="flex flex-col text-center text-white py-8">
+        <span>
+          Data provided by{' '}
+          <a
+            href="https://developer.marvel.com"
+            className="font-bold underline"
+            noferreral
+          >
+            Marvel Api
+          </a>
+        </span>
+        <span>@{year}</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
