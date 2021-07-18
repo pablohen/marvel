46d0c945ebf48ea1d9991f2c58be3a4154fc8ddb
Pablo Henrique <pablo_hen@hotmail.com>
2021-07-18 12:45:46 -0300

    content showcase

---
+++ components/Showcase.js
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Showcase = ({ category, data }) => {
+  const categoryUrl = category.toLowerCase();
+  return (
+    <section className="space-y-4 pb-4">
+      <span className="text-3xl mx-4">{category}</span>
+
+      <div className="overflow-x-auto">
+        <div className="flex space-x-2">
+          {data.map(({ id, title, thumbnail }) => (
+            <Link href={`/${categoryUrl}/${id}`} passHref key={id}>
+              <a>
+                <div className="flex flex-col relative">
+                  {thumbnail && (
+                    <div className="w-48">
+                      <Image
+                        src={`${thumbnail.path}.${thumbnail.extension}`}
+                        alt={title}
+                        className=""
+                        layout="responsive"
+                        width={200}
+                        height={300}
+                      />
+                    </div>
+                  )}
+                  <span className="p-2 text-sm font-bold text-white text-right bg-black bg-opacity-50 absolute bottom-0 left-0 right-0">
+                    {title}
+                  </span>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Showcase;
