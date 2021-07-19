1b95562c2b95ff8c7fd8582d7fc378a55f62be89
Pablo Henrique <pablo_hen@hotmail.com>
2021-07-19 10:59:16 -0300

    removed SSR

--- pages/characters/[characterId].js
+++ pages/characters/[characterId].js
@@ -4,9 +4,21 @@ import Showcase from '../../components/Showcase';
 import Image from 'next/image';
 import moment from 'moment';
 import Footer from '../../components/Footer';
+import { useRouter } from 'next/router';
+import useCharacter from '../../hooks/useCharacter';
+import useMedia from '../../hooks/useMedia';
 
-const CharacterPage = ({ character, comics, events, series }) => {
+const CharacterPage = () => {
+  const router = useRouter();
+  const { characterId } = router.query;
+  const [character] = useCharacter(characterId);
+
   const { id, name, description, thumbnail, modified } = character;
+
+  const [comics] = useMedia(id, marvelApi.getCharacterComics);
+  const [events] = useMedia(id, marvelApi.getCharacterEvents);
+  const [series] = useMedia(id, marvelApi.getCharacterSeries);
+
   const modifiedDate = moment(modified).format('YYYY/MM/DD hh:mm:ss');
 
   return (
@@ -15,15 +27,17 @@ const CharacterPage = ({ character, comics, events, series }) => {
 
       <div className="w-full">
         <div className="flex flex-col md:flex-row space-x-0 md:space-x-4 space-y-4 md:space-y-0 mx-4">
-          <div className="w-full">
-            <Image
-              src={`${thumbnail.path}.${thumbnail.extension}`}
-              alt={name}
-              width={200}
-              height={200}
-              layout="responsive"
-            />
-          </div>
+          {thumbnail && (
+            <div className="w-full">
+              <Image
+                src={`${thumbnail.path}.${thumbnail.extension}`}
+                alt={name}
+                width={200}
+                height={200}
+                layout="responsive"
+              />
+            </div>
+          )}
 
           <div className="space-y-4">
             <h3 className="text-4xl font-bold">{name}</h3>
@@ -45,18 +59,18 @@ const CharacterPage = ({ character, comics, events, series }) => {
   );
 };
 
-export const getServerSideProps = async (ctx) => {
-  const { characterId } = ctx.params;
-  const character = await marvelApi.getCharacter(characterId);
-  const { id } = character;
+// export const getServerSideProps = async (ctx) => {
+//   const { characterId } = ctx.params;
+//   const character = await marvelApi.getCharacter(characterId);
+//   const { id } = character;
 
-  const comics = await marvelApi.getCharacterComics(id);
-  const events = await marvelApi.getCharacterEvents(id);
-  const series = await marvelApi.getCharacterSeries(id);
+//   const comics = await marvelApi.getCharacterComics(id);
+//   const events = await marvelApi.getCharacterEvents(id);
+//   const series = await marvelApi.getCharacterSeries(id);
 
-  return {
-    props: { character, comics, events, series },
-  };
-};
+//   return {
+//     props: { character, comics, events, series },
+//   };
+// };
 
 export default CharacterPage;
