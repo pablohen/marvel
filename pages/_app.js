b20c6593cd2b9db65c00c5a81965ec90822a2871
Pablo Henrique <pablo_hen@hotmail.com>
2021-07-19 12:58:40 -0300

    removed extra ';'

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...SEO} />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
