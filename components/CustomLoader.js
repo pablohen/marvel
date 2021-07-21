3427d1b230ab1e0f70dd6db9f1a853d1ee0e0bd3
Pablo Henrique <pablo_hen@hotmail.com>
2021-07-21 11:46:36 -0300

    h alignment changes

--- components/CustomLoader.js
+++ components/CustomLoader.js
@@ -2,7 +2,7 @@ import Loader from 'react-loader-spinner';
 
 const CustomLoader = ({ text }) => {
   return (
-    <div className="flex flex-col space-y-2">
+    <div className="flex flex-col items-center space-y-2">
       <span className="font-bold">{text}</span>
       <Loader type="Circles" color="gray" />
     </div>
