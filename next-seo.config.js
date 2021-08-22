18d46a7c8d140eca5cda664d0857d7f825d877e2
Pablo Henrique <pablo_hen@hotmail.com>
2021-08-22 08:27:26 -0300

    starting with strict mode off

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,10 @@
 const site = `https://${process.env.VERCEL_URL}`;
 const siteName = 'Marvel API';
 const description = 'Marvel API demo using React.';
-const keywords = 'Marvel, pablohen, next, react, js, tailwind, css';
 const nextSeo = {
   defaultTitle: siteName,
   titleTemplate: `%s | ${siteName}`,
   description,
-  keywords,
   openGraph: {
     type: 'website',
     locale: 'en-US',
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,11 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&display=swap"
             rel="stylesheet"
